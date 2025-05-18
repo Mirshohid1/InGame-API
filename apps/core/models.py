02f32a3488ba10ff5a5f7928493ae5a765c27d66
Mirshohid1 <mirshohid1214@gmail.com>
2025-05-18 16:20:16 +0500

    feat(apps/core): update BaseModel

--- apps/core/models.py
+++ apps/core/models.py
@@ -1,10 +1,25 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
+from .utils import clean_text_fields
+
 
 class BaseModel(models.Model):
     created_at = models.DateTimeField(verbose_name=_('Created At'), auto_now_add=True)
     updated_at = models.DateTimeField(verbose_name=_('Updated At'), auto_now=True)
+
+    def clean(self):
+        for field in self._meta.fields:
+            if (isinstance(field, models.CharField) or 
+                isinstance(field, models.TextField) or
+                isinstance(field, models.EmailField)
+                ):
+                value = getattr(self, field.name)
+                setattr(self, field.name, clean_text_fields(value))
+    
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
 
     class Meta:
         abstract = True
